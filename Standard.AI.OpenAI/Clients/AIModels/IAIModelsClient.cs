9352aa2c551884ee76b40d6b793695efdfe85261
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-04-11 12:00:48 +0500

    CODE RUB: Fix Comments

--- Standard.AI.OpenAI/Clients/AIModels/IAIModelsClient.cs
+++ Standard.AI.OpenAI/Clients/AIModels/IAIModelsClient.cs
@@ -11,16 +11,6 @@ namespace Standard.AI.OpenAI.Clients.AIModels
 {
     public interface IAIModelsClient
     {
-        /// <summary>
-        /// Lists the currently available models, and provides basic information
-        /// about each one such as the owner and availability.
-        /// </summary>
-        /// <returns>
-        /// A <see cref="ValueTask{TResult}"/> that represents the result of the
-        /// asynchronous request, a list of <see cref="AIModel"/>s which
-        /// represents the basic information about the currently available
-        /// models.
-        /// </returns>
         /// <exception cref="AIModelClientValidationException" />
         /// <exception cref="AIModelClientDependencyException" />
         /// <exception cref="AIModelClientServiceException" />
